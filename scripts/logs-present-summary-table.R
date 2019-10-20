0bac7f42e8c42985a0a223c5d3576262b00f34e0
hejtmy <hejtmy@gmail.com>
2019-10-20 02:46:17 +0200

    Updates a bit the script!

--- scripts/logs-present-summary-table.R
+++ scripts/logs-present-summary-table.R
@@ -6,7 +6,8 @@ folder_output <- function(folder){
                    id=character(), type=character(), 
                    date=character(), time=character(),
                    player_log_length = numeric(),
-                   stringsAsFactors = FALSE)
+                   stringsAsFactors = FALSE,
+                   items_picked = numeric())
   exps <- load_experiments(folder)
   for(exp in exps){
     result <- list()
@@ -16,6 +17,7 @@ folder_output <- function(folder){
     t <- strptime(exp$timestamp, format = "%H-%M-%S-%d-%m-%Y")
     result$date <- strftime(t, format = "%x")
     result$time <- strftime(t, format = "%X")
+    result$items_picked <- nrow(exp$data$experiment_log$data)
     log_length <- tail(exp$data$position$data$time_since_start, 1)
     result$player_log_length <-  paste0(round(log_length/60,0), "m ", round(log_length %% 60, 0), "s")
     df <- base::rbind(df, as.data.frame(result))
@@ -26,19 +28,16 @@ folder_output <- function(folder){
 # script ----
 UNITY_DIR <- "E:/Google Drive/NUDZ/Projects/CDZ Karvina/VR-tasks/logy/1-pilot_VR-pen-paper/"
 TYPES <- c("house", "supermarket")
-
+type <- TYPES[2]
 pth <- file.path(UNITY_DIR, type)
 folders <- list.files(pth, full.names = TRUE)
 folders <- folders[!grepl("[.]ini", folders)] #removes potential .ini on windows
-
+#folders <- folders[1:4]
 df <- data.frame()
 for(type in TYPES){
-  
+  for(folder in folders){
+    output <- folder_output(folder)
+    df <- base::rbind(df, output)
+  }
 }
-
-for(folder in folders){
-  output <- folder_output(folder)
-  df <- base::rbind(df, output)
-}
-
 write.csv(df, "logs_present.csv", row.names = FALSE)
