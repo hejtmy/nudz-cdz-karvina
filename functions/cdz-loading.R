68ccc6e173f826de416b49d430edce6b86faeaa3
hejtmy <hejtmy@gmail.com>
2019-11-06 20:09:19 +0100

    adds loading funcitons

---
+++ functions/cdz-loading.R
@@ -0,0 +1,30 @@
+# Loads the participant data from given folder
+#' @param dir: directory of all the data
+#' @param type: monitor or VR
+#' @param id: participants id
+#' @param version: either settings version "A" or "B". Needs to be capitalized
+#' 
+#' WHY are tehre multiple exps???
+load_participant <- function(dir, id, tasklist, tasklist_version){
+  participant_folder <- file.path(dir, id)
+}
+
+load_participant_supermarket <- function(data_dir, id, tasklists){
+  participant_folder <- file.path(data_dir, "supermarket", id)
+  exps <- load_supermarket_experiments(participant_folder)
+  version <- exps[[1]]$data$experiment_info$Experiment$Settings$settings[17]
+  version <- gsub(".*verze ([A-Z])\\.json", "\\1", version)
+  for(i in 1:length(exps)){
+    exps[[i]]$tasklist <- tasklists[[version]]
+  }
+  return(exps)
+}
+
+load_tasklists <- function(data_dir) {
+  settingsA <- file.path(data_dir, "verze A.json")
+  settingsB <- file.path(data_dir, "verze B.json")
+  settingsA <- brainvr.supermarket::load_supermarket_takslist(settingsA)
+  settingsB <- brainvr.supermarket::load_supermarket_takslist(settingsB)
+  return(list(A = settingsA, B = settingsB))
+}
+
