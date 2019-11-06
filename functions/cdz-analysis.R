6c3e2ea5f0652ac4016c14cfc707c909a348611f
hejtmy <hejtmy@gmail.com>
2019-11-06 20:27:02 +0100

    Adds general supermarket results

---
+++ functions/cdz-analysis.R
@@ -0,0 +1,16 @@
+supermarket_results.participant <- function(participant){
+  results <- data.frame()
+  for(i in 1:length(participant)){
+    session <- participant[[i]]
+    df_session <- supermarket_results.session(session)
+    df_session$participant <- session$participant_id 
+    df_session$timestamp <- session$timestamp
+    results <- rbind(results, df_session)
+  }
+  return(results)
+}
+
+supermarket_results.session <- function(session){
+  df <- brainvr.supermarket::task_performance_all(session)
+  return(df)
+}
