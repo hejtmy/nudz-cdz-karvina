bda4bdb95e7c2d9d0490221417272dcc8b7ac8a0
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-01-24 03:43:33 +0100

    Updates buffering

--- scripts/buffer-online-tables.R
+++ scripts/buffer-online-tables.R
@@ -1,9 +1,15 @@
+source("functions/cdz-getters.R")
 df_demographics <- get_sheet_demographics()
-df_rbans <- get_sheet_rbans()
-df_ids <- get_sheet_ids()
-df_questionnaire <- get_sheet_questionnaire()
-
-write.table(df_ids, "information.csv", sep = ";", row.names = FALSE)
 write.table(df_demographics, "demographics.csv", sep = ";", row.names = FALSE)
+
+df_rbans <- get_sheet_rbans()
 write.table(df_rbans, "rbans.csv", sep = ";", row.names = FALSE)
+
+df_ids <- get_sheet_ids()
+write.table(df_ids, "information.csv", sep = ";", row.names = FALSE)
+
+df_questionnaire <- get_sheet_questionnaire()
 write.table(df_questionnaire, "questionnaire.csv", sep = ";", row.names = FALSE)
+
+df_sessions <- get_sheet_sessions()
+write.table(df_sessions, "sessions.csv", sep = ";", row.names = FALSE)
